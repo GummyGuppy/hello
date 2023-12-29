f5590c18bfe841c91d6bd9571975fefd9404818f
gummyguppy <alphastack.dev@gmail.com>
2023-12-28 18:59:22 -0600

    doc updates

--- lib/hello_web/controllers/hello_controller.ex
+++ lib/hello_web/controllers/hello_controller.ex
@@ -4,4 +4,16 @@ defmodule HelloWeb.HelloController do
   def index(conn, _params) do
     render(conn, :index)
   end
+
+  @doc """
+    The keys of the params map will always be strings,
+    and that the equals sign does not represent assignment,
+    but is instead a pattern match assertion.
+
+    %{"messenger" => messenger}
+
+  """
+  def show(conn, %{"messenger" => messenger}) do
+    render(conn, :show, messenger: messenger)
+  end
 end
