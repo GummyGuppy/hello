934ad8572e2453126adf59efff76385fa8cccd8c
gummyguppy <alphastack.dev@gmail.com>
2023-12-29 20:43:38 -0600

    added routes documentation

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -1,6 +1,20 @@
 defmodule HelloWeb.Router do
+
+  # The first line of this module, use HelloWeb, :router,
+  # simply makes Phoenix router functions available in our
+  # particular router.
   use HelloWeb, :router
 
+  # Pipelines are a series of plugs that can be attached to specific scopes.
+  # ex. pipeline :browser, :api
+  @doc """
+    The :browser pipeline has six plugs:
+    The plug :accepts, ["html"] defines the accepted request format or formats.
+    :fetch_session, which, naturally, fetches the session data and makes it available in the connection.
+    :fetch_live_flash, which fetches any flash messages from LiveView and merges them with the controller flash messages.
+    Then, the plug :put_root_layout will store the root layout for rendering purposes.
+    Later :protect_from_forgery and :put_secure_browser_headers, protects form posts from cross-site forgery.
+  """
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -15,12 +29,35 @@ defmodule HelloWeb.Router do
     plug :accepts, ["json"]
   end
 
+ # ~~ ROOT SCOPE ~~
   scope "/", HelloWeb do
+
+    #pipelines allow a set of plugs to be applied to different sets of routes.
     pipe_through :browser
 
+    # get is a Phoenix macro that corresponds to the HTTP verb GET.
+    # Similar macros exist for other HTTP verbs, including POST, PUT, PATCH, DELETE, OPTIONS, CONNECT, TRACE, and HEAD
     get "/", PageController, :home
+
+
+    resources "/users", UserController do
+      resources "/posts", PostController
+    end
+
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
+    resources "/reviews", ReviewController
+  end
+
+  # ~~ ADMIN SCOPE ~~
+  # We define a new scope where all routes are prefixed with /admin and all controllers are
+  # under the HelloWeb.Admin namespace.
+  scope "/admin", HelloWeb.Admin do
+    pipe_through :browser
+
+    resources "/images",  ImageController
+    resources "/reviews", ReviewController
+    resources "/users",   UserController
   end
 
   # Other scopes may use custom stacks.
